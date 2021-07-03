0b217e7ecd17ebb487c04cb56aa5a32ffb9c62f6
guozhi <guozhi.li@dmall.com>
2021-07-03 16:22:06 +0800

    export

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
 pub mod client;
 pub mod pkg;
+
+pub use client::sync::OffsetConfig;
+pub use client::sync::Runner;
+pub use mysql::Value;
+pub use mysql_binlog::EventIterator;
+pub use pkg::event::Event;
+pub use pkg::mysql_gtid::Gtid;
